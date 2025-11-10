54806ea432c5a7d6a722715fd0f9f08f2dd33240
Bigi <alessandro.biagiotti@studenti.unimi.it>
2025-11-10 17:29:36 +0100

    Initial commit, added the archrestore main bash file, added a data folder that will be used to keep track of the necessary packages plus some other things that, for the time being, I can't put anywhere else

---
+++ archrestore.sh
@@ -0,0 +1,8 @@
+# install yay
+
+# Use pacman to install all the necessary packages
+sudo pacman -S --needed - <data/pkglist.txt
+
+# Use yay to install the rest of the packages
+
+# Configuration of the packages
